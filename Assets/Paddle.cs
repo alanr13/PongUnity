a9bdfddec9a1349e8ca2d98ab6df9dc14cb5c4f6
alanrichter53@gmail.com <alanrichter53@gmail.com>
2025-08-05 16:04:26 +0200

    Made paddles bigger and added a ball object, haven't added movement to the ball yet.

--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -1,12 +1,16 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    public Transform paddle;
     private Vector2 direction;
+    private List<Transform> paddleSegments = new List<Transform>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
