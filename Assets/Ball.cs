7fc104d60f60a9a194c72764c93b533dcf805776
alanr13 <alanrichter53@gmail.com>
2025-08-24 13:45:09 +0200

    Added ball object

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    int rand;
+    Vector2 direction;
+    public float moveSpeed;
+    public Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rand = Random.Range(1, 3);
+
+        if (rand == 1)
+        {
+            direction = Vector3.right;
+        }
+        else
+        {
+            direction = Vector3.left;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + direction * moveSpeed);
     }
 }
